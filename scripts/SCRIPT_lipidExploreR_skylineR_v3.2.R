1a104ab9b4f3b5d505e59a29d12c4158e87e098b
Luke Whiley <luke.whiley@murdoch.edu.au>
2022-07-01 10:14:16 +0800

    updates

--- scripts/SCRIPT_lipidExploreR_skylineR_v3.2.R
+++ scripts/SCRIPT_lipidExploreR_skylineR_v3.2.R
@@ -24,6 +24,22 @@ master_list$summary_tables$project_summary <- setNames(master_list$summary_table
 #github master directory
 master_list$project_details$github_master_dir <- "https://raw.githubusercontent.com/lukewhiley/targeted_lipid_exploreR_v3/main"
 
+#setup project directories
+#data
+if(!dir.exists(paste0(master_list$project_details$project_dir, "/data"))){dir.create(paste0(master_list$project_details$project_dir, "/data"))}
+#mzml
+if(!dir.exists(paste0(master_list$project_details$project_dir, "/data/mzml"))){dir.create(paste0(master_list$project_details$project_dir, "/data/mzml"))}
+#rda
+if(!dir.exists(paste0(master_list$project_details$project_dir, "/data/rda"))){dir.create(paste0(master_list$project_details$project_dir, "/data/rda"))}
+#skyline
+if(!dir.exists(paste0(master_list$project_details$project_dir, "/data/skyline"))){dir.create(paste0(master_list$project_details$project_dir, "/data/skyline"))}
+#sciex
+if(!dir.exists(paste0(master_list$project_details$project_dir, "/data/sciex_raw"))){dir.create(paste0(master_list$project_details$project_dir, "/data/sciex_raw"))}
+#batch_correct
+if(!dir.exists(paste0(master_list$project_details$project_dir, "/data/batch_correction"))){dir.create(paste0(master_list$project_details$project_dir, "/data/batch_correction"))}
+#html_reports
+if(!dir.exists(paste0(master_list$project_details$project_dir, "/html_report"))){dir.create(paste0(master_list$project_details$project_dir, "/html_report"))}
+
 #read in mrm_guide from github
 master_list$mrm_guides$mrm_guide <- read_csv(
   paste0(master_list$project_details$github_master_dir,
@@ -33,12 +49,13 @@ master_list$mrm_guides$mrm_guide <- read_csv(
 #source functions
 #RT finder
 master_list$functions$mrm_RT_findeR_mzR <- source(paste0(
-  master_list$project_details$github_master_dir , "/functions/2022_LGW_FUNC_MRM_findeR_pwiz3019_mzR.R"))
+  master_list$project_details$github_master_dir , 
+  "/functions/FUNC_lipidExploreR_MRM_findeR_pwiz3019_mzR.R"))
 
 #peak_boundary findeR
 master_list$functions$mrm_pb_findeR <- source(paste0(
   master_list$project_details$github_master_dir, 
-  "/functions/2022_LGW_FUNC_peak_boundary_findeR.R"))
+  "/functions/FUNC_lipidExploreR_peak_boundary_findeR.R"))
 
 dlg_message("convert SCIEX files to mzML", type = 'ok'); dlg_message(paste0("mzML directory: [", paste0(master_list$project_details$project_dir, "/data/mzml"), "]"), type = 'ok'); dlg_message("put mzML files in sub folders per plate [/data/mzml/plate_1; /data/mzml/plate_2] etc")
 
@@ -81,7 +98,6 @@ for(idx_plate in master_list$project_details$mzml_plate_list){
   temp_mzR_list <- c(temp_mzR_list, master_list$data$mzR[[idx_plate]])
 }
 
-
 #######
 # Retention time optimiser
 master_list$mrm_guides$mrm_guide_rt_update <- tibble()
@@ -98,27 +114,22 @@ master_list$mrm_guides$mrm_guide_rt_update <- setNames(master_list$mrm_guides$mr
 
 rm(temp_mzR_list)
 
-
-#create directory for exporting skyline templates
-if(!dir.exists(paste0(master_list$project_details$project_dir, "/skyline_updates"))){
-  dir.create(paste0(master_list$project_details$project_dir, "/skyline_updates"))
-}
 #export updated optimised RT times
 write_csv(x = master_list$mrm_guides$mrm_guide_rt_update,
-          file = paste0(master_list$project_details$project_dir, "/skyline_updates/", Sys.Date(), "_", 
+          file = paste0(master_list$project_details$project_dir, "/data/skyline/", Sys.Date(), "_", 
                         master_list$project_details$project_name, "_mrm_skylineR_RT_update.csv"))
 
 #create directory for storing skyline exports
-if(!dir.exists(paste0(master_list$project_details$project_dir, "/skyline_reports"))){
-  dir.create(paste0(master_list$project_details$project_dir, "/skyline_reports"))
+if(!dir.exists(paste0(master_list$project_details$project_dir, "/data/skyline"))){
+  dir.create(paste0(master_list$project_details$project_dir, "/data/skyline"))
 }
 
 #interact with skyline
-dlg_message("1. Please open skylineMS software", type = 'ok');dlg_message("2. Create new small molecule file", type = 'ok'); dlg_message("3. Import the [skylineR_RT_update.csv] transition list located in [/skyline_updates] folder. In Skyline navigate to File -> import -> transition list", type = 'ok'); dlg_message("4. Save project", type = 'ok'); dlg_message("5. Import mzml data files for processing by navigating to File -> import -> results", type = 'ok'); dlg_message("6. Let skyline process.  Export results to [/skyline_reports] folder with the tag xSkylineR_1.  Export reports must have the following headings: Replicate, Protein, Peptide, Area, Retention Time, Start Time and End Time", type = 'ok'); dlg_message("7. Now return to R Studio", type = 'ok'); dlg_message("Next select the export file for importing into skylineR", type = 'ok')
+dlg_message("1. Please open skylineMS software", type = 'ok');dlg_message("2. Create new small molecule file", type = 'ok'); dlg_message("3. Import the [skylineR_RT_update.csv] transition list located in [~project_directory/data/skyline] folder. In Skyline navigate to File -> import -> transition list", type = 'ok'); dlg_message("4. Save project", type = 'ok'); dlg_message("5. Import mzml data files for processing by navigating to File -> import -> results", type = 'ok'); dlg_message("6. Let skyline process.  Export results to [/skyline_reports] folder with the tag xSkylineR_1.  Export reports must have the following headings: Replicate, Protein, Peptide, Area, Retention Time, Start Time and End Time", type = 'ok'); dlg_message("7. Now return to R Studio", type = 'ok'); dlg_message("Next select the export file for importing into skylineR", type = 'ok')
 
 #re_import skyline file
 master_list$data$skyline_reports$report_1 <- read_csv(file = paste0(list.files(
-  paste0(master_list$project_details$project_dir, "/skyline_reports"),
+  paste0(master_list$project_details$project_dir, "/data/skyline"),
   pattern = "xSkylineR_1", full.names = TRUE)), show_col_types = FALSE)
 
 #perform peak boundary update
@@ -136,15 +147,15 @@ master_list$mrm_guides$mrm_guide_pb_update_all_plates <- bind_rows(master_list$m
 
 #write peak boundary output
 write_csv(x = master_list$mrm_guides$mrm_guide_pb_update_all_plates,
-          file = paste0(master_list$project_details$project_dir, "/skyline_updates/", Sys.Date(), "_", 
+          file = paste0(master_list$project_details$project_dir, "/data/skyline/", Sys.Date(), "_", 
                         master_list$project_details$project_name, "_mrm_peak_boundary_updated.csv"))
 
-dlg_message("1. Please return to skylineMS software", type = 'ok'); dlg_message("2. Import the new skylineR_boundary_update.csv transition list from csv file by navigating to File -> import -> peak boundaries", type = 'ok'); dlg_message("4. Save project", type = 'ok');dlg_message("5. Export results to [/skyline_reports] folder with the tag xSkylineR_2.  Export reports must have the following headings: Replicate, Protein, Peptide, Area, Retention Time, Start Time and End Time", type = 'ok'); dlg_message("7. Now return to R Studio and run the lipid_exploreR to QC check data", type = 'ok')
+dlg_message("1. Please return to skylineMS software", type = 'ok'); dlg_message("2. Import the new skylineR_boundary_update.csv transition list from csv file by navigating to File -> import -> peak boundaries", type = 'ok'); dlg_message("4. Save project", type = 'ok');dlg_message("5. Export results to [~project_directory/data/skyline] folder with the tag xSkylineR_2.  Export reports must have the following headings: Replicate, Protein, Peptide, Area, Retention Time, Start Time and End Time", type = 'ok'); dlg_message("7. Now return to R Studio and run the lipid_exploreR to QC check data", type = 'ok')
 
 #import processed skyline_data
 #re_import skyline file
 master_list$data$skyline_reports$report_2 <- read_csv(file = paste0(list.files(
-  paste0(master_list$project_details$project_dir, "/skyline_reports"),
+  paste0(master_list$project_details$project_dir, "/data/skyline"),
   pattern = "xSkylineR_2", full.names = TRUE)), show_col_types = FALSE) %>% clean_names()
 
 #create directory for exporting rda files
