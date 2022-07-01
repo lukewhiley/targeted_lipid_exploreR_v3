17156f9a99d43a40d757c0e18eb350f7118a7b0b
Luke Whiley <luke.whiley@murdoch.edu.au>
2022-07-01 14:13:56 +0800

    Update SCRIPT_lipidExploreR_skylineR_v3.2.R

--- scripts/SCRIPT_lipidExploreR_skylineR_v3.2.R
+++ scripts/SCRIPT_lipidExploreR_skylineR_v3.2.R
@@ -2,7 +2,12 @@
 dlg_message("Welcome to skylineR! :-)", type = 'ok')
 
 #set up project master list
-master_list <- list(); master_list$project_details <- list(); master_list$functions <- list();  master_list$mrm_guides <- list(); master_list$data <- list(); master_list$data$mzR <- list(); master_list$data$skyline_reports <- list(); master_list$summary_tables <- list()
+master_list <- list(); master_list$environment <- list(); master_list$project_details <- list(); master_list$functions <- list();  master_list$mrm_guides <- list(); master_list$data <- list(); master_list$data$mzR <- list(); master_list$data$skyline_reports <- list(); master_list$summary_tables <- list()
+
+#store environment details
+master_list$environment <- sessionInfo()$R.version$version.string
+master_list$base_packages <- sessionInfo()$basePkgs
+master_list$user_packages <- paste0(names(sessionInfo()$otherPkgs), ": ", paste0(installed.packages()[names(sessionInfo()$otherPkgs), "Version"]))
 
 ##USER INPUT##
 #set project details
