7dfb00b22b10bb824b3bcbac80e3e3321071f2bd
Luke Whiley <luke.whiley@murdoch.edu.au>
2022-07-04 18:03:56 +0800

    Update FUNC_lipidExploreR_conc_calculator.R

--- functions/FUNC_lipidExploreR_conc_calculator.R
+++ functions/FUNC_lipidExploreR_conc_calculator.R
@@ -1,61 +1,26 @@
 #impute function
 
 lgw_lipid_conc_calc <- function(FUNC_data,
-                              FUNC_metabolite_list,
-                             FUNC_SIL_guide_path,
-                             FUNC_conc_guide_path)
-  {
-
- FUNC_list <- list()
+                                FUNC_metabolite_list,
+                                FUNC_SIL_guide,
+                                FUNC_conc_guide)
+{
   
-  FUNC_SIL_guide <- read_csv(
-   file = FUNC_SIL_guide_path, 
-    show_col_types = FALSE) %>% 
-    clean_names()
+  FUNC_out <- FUNC_data %>% select(contains("sample"))
   
-
-  FUNC_conc_guide <- read_csv(file = FUNC_conc_guide_path, show_col_types = FALSE) %>% 
-    clean_names()
+  FUNC_sil_list <- unique(FUNC_conc_guide$sil_name)
   
-lipid_features <- FUNC_metabolite_list[!grepl("SIL",FUNC_metabolite_list)] 
-    
-    #set empty list
-FUNC_list$out <- FUNC_data %>%
-      select(contains("sample"))
-    
-    for(idx_lipid in lipid_features){
-      #find appropriate SIL IS from SIL_guide
-      SIL_used <- FUNC_SIL_guide %>%
-        filter(precursor_name == idx_lipid) %>%
-        select(note) %>% paste0()
-      
-      SIL_idx <- which(names(FUNC_data) == SIL_used) # find SIL in dataset. If it has been removed the next phase of the function will be skipped.
-      
-      if(length(SIL_idx) == 1){
-      #find SIL concentration factor from template
-      SIL_concentration_factor <- FUNC_conc_guide %>%
-        filter(sil_name == all_of(SIL_used)) %>%
-        select(concentration_factor) %>% as.numeric()
-      
-      FUNC_list$out <- FUNC_list$out %>% 
-        #left join to keep samples and rows aligned
-        left_join(FUNC_data %>%
-                    #select lipid and its SIL IS columns
-                    select(sample_name, all_of(idx_lipid), all_of(SIL_used)) %>% 
-                    #create ratio between lipid and SIL IS
-                    add_column("ratio" = (FUNC_data[[idx_lipid]]/
-                                            FUNC_data[[SIL_used]])*
-                                 #multiply by concentration factor (from template)
-                                 SIL_concentration_factor) %>%
-                    #drop lipid and ratio columns
-                    select(-all_of(idx_lipid), -all_of(SIL_used)) %>% 
-                    #rename column
-                    rename_with(~all_of(idx_lipid), "ratio"),
-                  by= "sample_name"
-        )
-      }
-    }
+  for(idx_sil in FUNC_sil_list){
+    if(length(which(names(FUNC_data) == idx_sil))==1){ #only perform the below if SIL is present in data (sometimes may be filtered out)
+    target_lipids <- FUNC_data %>% select(any_of((FUNC_SIL_guide %>% filter(note == idx_sil) %>% select(precursor_name))[[1]])) #find target lipids that use defined SIL IS
+    if(ncol(target_lipids) > 0){
+    response_ratio <- (target_lipids/FUNC_data[[idx_sil]]) #calculate response ratio
+    #calculate concentration from diltution factor
+    concentration <- (response_ratio * (FUNC_conc_guide %>% 
+      filter(sil_name == all_of(idx_sil)) %>%
+      select(concentration_factor) %>% as.numeric())) %>% as_tibble()
+    FUNC_list <- cbind(FUNC_out, concentration)
+    }}}
 
-FUNC_list$out
+  FUNC_list$out
 }
-  
